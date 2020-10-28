b7db0d520f62a835073c6083a3d7513e9951b195
Quinn <quinn_x@126.com>
2020-10-28 23:28:09 +0800

    [SK]添加Rx

--- GeekCalculator/GeekCalculator/RxViewController.swift
+++ GeekCalculator/GeekCalculator/RxViewController.swift
@@ -1,14 +1,16 @@
 //
-//  ViewController.swift
+//  RxViewController.swift
 //  GeekCalculator
 //
 //  Created by Quinn on 2020/10/28.
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import RxSwift
+import RxCocoa
+import RxFeedback
+class RxViewController: UIViewController {
+    var disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ GeekCalculator/Pods/Target Support Files/Pods-GeekCalculator/Pods-GeekCalculator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GeekCalculator : NSObject
+@end
+@implementation PodsDummy_Pods_GeekCalculator
+@end
