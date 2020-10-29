4e0d8647d9c4316e52fb4a5d4ca1a821b14c5396
Quinn <quinn_x@126.com>
2020-10-30 00:53:21 +0800

    [SK]完善计算逻辑

--- GeekCalculator/GeekCalculator/Calculator/Calculator.swift
+++ GeekCalculator/GeekCalculator/Calculator/Calculator.swift
@@ -31,6 +31,7 @@ enum CalculatorState {
 
 // 是否有效点击了 =
 var CalculatorClickEqual = false
+var CalculatorFloatCount = 0
 
 extension CalculatorState {
     static let initial = CalculatorState.oneOperand(screen: "0")
@@ -88,17 +89,17 @@ extension CalculatorState {
             return state.mapScreen { $0.effective.range(of: ".") == nil ? $0 + "." : $0 }
         case .changeSign:
             return state.mapScreen { char -> String in
-                if char.count > 0,Int(char.effective) != 0{
+                if char.count > 0,Int(char.effective) != 0,Int(char.effective) != nil{
                     let array = char.split(separator: " ")
                     if array.count > 1{
                         var temp = char
                         temp.removeLast()
-                        return temp + "\(-(Double(char.effective) ?? 0.0))"
+                        return temp + String(format: "%.\(CalculatorFloatCount)f", -(Double(char.effective) ?? 0))
                     }else{
-                        return "\(-(Double(char.effective) ?? 0.0))"
+                        return String(format: "%.\(CalculatorFloatCount)f", -(Double(char.effective) ?? 0))
                     }
                 }else{
-                    return "0"
+                    return char
                 }
             }
         case .percent:
@@ -108,9 +109,17 @@ extension CalculatorState {
                     if array.count > 1{
                         var temp = char
                         temp.removeLast()
-                        return temp + "\((Double(char.effective) ?? 0.0) / 100.0)"
+                        if char.hasDecimals{
+                            return temp + String(format: "%.\(CalculatorFloatCount)f", (Double(char.effective) ?? 0.0) / 100.0)
+                        }else{
+                            return temp + String(format: "%.0f", (Double(char.effective) ?? 0.0) / 100.0)
+                        }
                     }else{
-                        return "\((Double(char.effective) ?? 0.0) / 100.0)"
+                        if char.hasDecimals{
+                            return String(format: "%.\(CalculatorFloatCount)f", (Double(char.effective) ?? 0.0) / 100.0)
+                        }else{
+                            return String(format: "%.0f", (Double(char.effective) ?? 0.0) / 100.0)
+                        }
                     }
                 }else{
                     return "0"
@@ -120,21 +129,32 @@ extension CalculatorState {
             switch state {
             case let .oneOperand(screen):
                 if CalculatorClickEqual{
-                    return .oneOperandAndOperator(operand: screen.doubleValue, operator: o, screen: screen.effective + o.sign)
+                    return .oneOperandAndOperator(operand: screen.effective.doubleValue, operator: o, screen: screen.effective + o.sign)
                 }else{
                     return .oneOperandAndOperator(operand: screen.doubleValue, operator: o, screen: screen + o.sign)
                 }
             case let .oneOperandAndOperator(operand, _,screen):
-                return .oneOperandAndOperator(operand: operand, operator: o, screen: screen + o.sign)
+                let newscreen = screen.removeToNumber
+                return .oneOperandAndOperator(operand: operand, operator: o, screen: newscreen + o.sign)
             case let .twoOperandsAndOperator(operand, oldOperator, screen):
-                return .twoOperandsAndOperator(operand: oldOperator.perform(operand, screen.effective.doubleValue), operator: o, screen: screen + o.sign)
+                let newscreen = screen.removeToNumber
+                return .twoOperandsAndOperator(operand: oldOperator.perform(operand, screen.effective.doubleValue), operator: o, screen: newscreen + o.sign)
             }
         case .equal:
             switch state {
             case let .twoOperandsAndOperator(operand, operat, screen):
                 let result = operat.perform(operand, screen.effective.doubleValue)
                 CalculatorClickEqual = true
-                return .oneOperand(screen: screen + " = " + String(result))
+                if operat != .division{
+                    if screen.hasDecimals{
+                        return .oneOperand(screen: screen + " = " + String(format: "%.\(CalculatorFloatCount)f", result))
+                    }else{
+                        return .oneOperand(screen: screen + " = " + String(format: "%.0f", result))
+                    }
+                }else{
+                    return .oneOperand(screen: screen + " = " + String(format: "%.4f", result))
+                }
+                
             default:
                 return state
             }
@@ -150,6 +170,34 @@ extension String{
             return self
         }
     }
+    var hasDecimals:Bool{
+        let arr = self.split(separator: " ")
+        var isDecimals = false
+        CalculatorFloatCount = 0
+        for item in arr{
+            let childs = item.split(separator: ".")
+            if childs.count > 1,Int(childs[1])! > 0{
+                CalculatorFloatCount = max(childs[1].count, CalculatorFloatCount)
+                isDecimals =  true
+            }
+        }
+        return isDecimals
+    }
+    //"3 + " 移除 " + "
+    var removeThree:String{
+        var new = self
+        new.removeLast()
+        new.removeLast()
+        new.removeLast()
+        return new
+    }
+    var removeToNumber:String{
+        var new = self
+        while Int(String(new.last!)) == nil {
+            new.removeLast()
+        }
+        return new
+    }
 }
 extension Operator {
     var sign: String {
@@ -157,7 +205,7 @@ extension Operator {
         case .addition:         return " + "
         case .subtraction:      return " - "
         case .multiplication:   return " × "
-        case .division:         return " / "
+        case .division:         return " ÷ "
         }
     }
     
